f98905cbca635ab27c4ee66ef45c671a78619f3e
Michal Hoffman <michal.z.hoffman@gmail.com>
2024-11-14 16:57:07 +0000

    module 03

--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -1,10 +1,10 @@
-import colors from "@/constants/colors";
-import { MaterialIcons } from "@expo/vector-icons";
-import { TabTriggerSlotProps } from "expo-router/build/ui";
-import { ComponentProps, Ref, forwardRef } from "react";
-import { Text, Pressable, View } from "react-native";
+import colors from '@/constants/colors';
+import { MaterialIcons } from '@expo/vector-icons';
+import { TabTriggerSlotProps } from 'expo-router/build/ui';
+import { ComponentProps, Ref, forwardRef } from 'react';
+import { Text, Pressable, View } from 'react-native';
 
-type Icon = ComponentProps<typeof MaterialIcons>["name"];
+type Icon = ComponentProps<typeof MaterialIcons>['name'];
 
 export type TabButtonProps = TabTriggerSlotProps & {
   icon?: Icon;
@@ -15,10 +15,21 @@ export const TabButton = forwardRef(function TabButtonComponent(
   ref: Ref<View>
 ) {
   return (
-    <Pressable ref={ref} {...props}>
-      <View className="justify-between items-center gap-y-1 px-2 flex-col">
-        <MaterialIcons name={icon} size={24} />
-        <Text className={"text-md"}>{children}</Text>
+    <Pressable
+      ref={ref}
+      {...props}
+      className={isFocused ? ' sm:border-b-tint sm:border-b-2' : ''}
+    >
+      <View className='justify-between items-center gap-y-1 px-2 flex-col'>
+        <MaterialIcons name={icon} size={24} className='sm:hidden' />
+        <Text
+          className={
+            'text-sm sm:text-lg' +
+            (isFocused ? ' color-tint sm:color-black' : '')
+          }
+        >
+          {children}
+        </Text>
       </View>
     </Pressable>
   );
